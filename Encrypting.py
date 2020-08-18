749b9318d5db80f03a63b47174ad492f397b95d8
Jacomus782 <Jacomus782@gmail.com>
2020-08-18 18:01:11 +0100

    Gitignore and find method
    Added idea of how to find the particular numbers

--- Encrypting.py
+++ Encrypting.py
@@ -1,20 +1,40 @@
 # The first step is to generate lists of Letters
 
-Ualpha = list("ABECDEFGHIJKLMNOPQRSTUVWXYZ")
-Lalpha = list("abcdefghijklmnopqrstuvwxyz")
-num = list("1234567890")
+Ualpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+Lalpha = "abcdefghijklmnopqrstuvwxyz"
+num = "1234567890"
 
-Password = list(input('Type password here'))
-Key = list(input('Type Key here'))
+Password = input('Type password here')
+Key = input('Type Key here')
 key_len = len(Key)
 pas_len = len(Password)
 
+X = []
 for i in range(pas_len):
-    if 'Password[i]' in Ualpha:
-        Ualpha.find(str(Password[i]))
+    if Password[i] in Ualpha:
+        X.append(Ualpha.find(Password[i]))
+        print(X)
+    if Password[i] in Lalpha:
+        X.append(Lalpha.find(Password[i])+26)
+        print(X)
+    if Password[i] in num:
+        X.append(num.find(Password[i])+52)
+        print(X)
 
-    if 'Password[i]' in Lalpha:
-        Lalpha.find('Password[i]')
 
-    if 'Password[i]' in num:
-        num.find('Password[i]')
+Y = []
+for i in range(key_len):
+    if Key[i] in Ualpha:
+        y = Ualpha.find(Key[i])
+        print(y)
+        Y += str(y)
+    if Key[i] in Lalpha:
+        y = Lalpha.find(Key[i])
+        print(y)
+        Y += str(y)
+    if Key[i] in num:
+        y = num.find(Key[i])
+        print(y)
+        Y += str(y)
+print(Y)
+
